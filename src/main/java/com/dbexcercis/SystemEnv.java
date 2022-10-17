4d82b076f85d276c0c7cc3737486127ea55dbb1f
KimGunWoo9595 <medneter@naver.com>
2022-10-17 13:17:25 +0900

    첫 스프림 커밋

---
+++ src/main/java/com/dbexcercis/SystemEnv.java
@@ -0,0 +1,4 @@
+package com.dbexcercis;
+
+public class SystemEnv {
+}
---
+++ src/main/java/com/dbexcercis/UserDao.java
@@ -0,0 +1,34 @@
+package com.dbexcercis;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class UserDao {
+
+    public void add() throws SQLException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id,name,password) VALUES(?,?,?)");
+        ps.setString(1, "1");
+        ps.setString(2,"kim");
+        ps.setString(3,"1123");
+
+        int status = ps.executeUpdate();
+        System.out.println(status);
+        ps.close();
+        conn.close();
+
+    }
+
+    public static void main(String[] args) throws SQLException {
+        UserDao userDao = new UserDao();
+        userDao.add();
+    }
+}
