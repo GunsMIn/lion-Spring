b6f8d8483f857ad89d7be41b1d521b00a439c5d2
KimGunWoo9595 <medneter@naver.com>
2022-10-18 16:16:26 +0900

    팩토리 클래스(객체사용)

--- src/main/java/com/lion/refactoring4/UserDaoTest.java
+++ src/main/java/com/lion/refactoring4/UserDaoTest.java
@@ -5,7 +5,9 @@ import com.lion.domain.User;
 import java.sql.SQLException;
 
 public class UserDaoTest {
-    //팩토리사용 클래스 // test클래스
+    //팩토리사용 클래스
+    //UserDaoTest클래스의 본래의 성격은 UserDao를 test하는 목적의 클래스였다
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         // new DaoFactory().userDao().add(new User("15","박땡땡","32323"));
         UserDao userDao = new DaoFactory().userDao();
