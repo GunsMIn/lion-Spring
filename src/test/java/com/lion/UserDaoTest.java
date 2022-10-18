5bb7e91ffd31a4668ba92adc82fca8551a0d985a
KimGunWoo9595 <medneter@naver.com>
2022-10-18 11:27:36 +0900

    테스트 수정

--- src/test/java/com/lion/UserDaoTest.java
+++ src/test/java/com/lion/UserDaoTest.java
@@ -1,5 +1,25 @@
+package com.lion;
+
+import com.lion.domain.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class UserDaoTest {
+    //수정
+    @Test
+    void addAndSelect() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+        User user = new User("8", "라이언", "1123");
+        userDao.add(user);
+
+        User selectedUser = userDao.findById("8");
+        Assertions.assertEquals("라이언", selectedUser.getName());
+
+    }
 
 }
