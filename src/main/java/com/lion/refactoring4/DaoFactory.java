6d4ad4400ebba555acadbad58223b9f09450b1e8
KimGunWoo9595 <medneter@naver.com>
2022-10-18 15:55:06 +0900

    팩토리 클래스

--- src/main/java/com/lion/refactoring4/DaoFactory.java
+++ src/main/java/com/lion/refactoring4/DaoFactory.java
@@ -2,6 +2,7 @@ package com.lion.refactoring4;
 
 public class DaoFactory {
     //팩토리 클래스
+    //여기서 주입
     public UserDao userDao() {
         //어떤 디비를 사용 할 것인지 팩토리 클래스에서 정함
         ConnectionMaker connectionMaker = new DConnectionMaker();
