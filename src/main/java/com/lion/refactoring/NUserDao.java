084692433fd05f8ce82d4676cc0e59ffcb22bc3c
KimGunWoo9595 <medneter@naver.com>
2022-10-18 11:41:16 +0900

    abstract를 이용한 다형성 활용 add메소드

--- src/main/java/com/lion/refactoring/NUserDao.java
+++ src/main/java/com/lion/refactoring/NUserDao.java
@@ -1,4 +1,33 @@
 package com.lion.refactoring;
 
-public class NUserDao {
+import com.lion.domain.User;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class NUserDao extends UserDao{
+    
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+        return conn;
+    }
+
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        NUserDao n = new NUserDao();
+
+        n.add(new User("10","김땡땡","123423"));
+
+
+
+    }
 }
