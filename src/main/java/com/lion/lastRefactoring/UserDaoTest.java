5e96fac9c6a10bfdcd57c0d57b5f1eb54178b387
KimGunWoo9595 <medneter@naver.com>
2022-10-20 13:23:48 +0900

    deleteAll 메소드 테스ㅡ

--- src/main/java/com/lion/lastRefactoring/UserDaoTest.java
+++ src/main/java/com/lion/lastRefactoring/UserDaoTest.java
@@ -1,12 +1,15 @@
-package com.lion.lastRefact;
+package com.lion.lastRefactoring;
 
 import com.lion.domain.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,13 +18,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserDaoTest {
 
     @Autowired
-    ApplicationContext context;
+    ApplicationContext context; //
 
+    //deleteALL까지 테스트 !
     @Test
-    void addAndGet() {
+    @DisplayName("add,get,deleteAll메소드 추가")
+    void addAndGet() throws SQLException, ClassNotFoundException {
         UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+        userDao.deleteAll();
+        assertEquals(0,userDao.getCount());
+
         String id = "29";
         userDao.add(new User(id, "EternityHwan", "1234"));
+        assertEquals(1,userDao.getCount());
         User user = userDao.findById(id);
 
         assertEquals("EternityHwan", user.getName());
