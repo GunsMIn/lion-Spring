b868289b93bbeed8e74c15bead45bb4ad29caab5
KimGunWoo9595 <medneter@naver.com>
2022-10-20 14:08:08 +0900

    @BeforEach 적용

--- src/main/java/com/lion/lastRefactoring/UserDaoTest.java
+++ src/main/java/com/lion/lastRefactoring/UserDaoTest.java
@@ -1,11 +1,13 @@
 package com.lion.lastRefactoring;
 
 import com.lion.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -20,11 +22,19 @@ class UserDaoTest {
     @Autowired
     ApplicationContext context; //
 
+
+    UserDao userDao;
+
+    @BeforeEach
+    void setUp() {
+         this.userDao = context.getBean("awsUserDao", UserDao.class);
+    }
+
+
     //deleteALL까지 테스트 !
     @Test
     @DisplayName("add,get 메소드")
     void addAndGet() throws SQLException, ClassNotFoundException {
-        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
         User user1 = new User("1", "김수로", "1123");
 
         userDao.add(user1);
@@ -36,10 +46,10 @@ class UserDaoTest {
 
 
 
+
     @Test
     @DisplayName("deleteAll메소드와 Add 테스트")
     public void count() throws SQLException, ClassNotFoundException {
-        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
         User user1 = new User("30", "김영기", "1234");
         User user2 = new User("31", "김건우", "1234");
         User user3 = new User("32", "김건희", "1234");
@@ -57,4 +67,16 @@ class UserDaoTest {
         assertEquals(3,userDao.getCount());
 
     }
+
+
+    @Test
+    void findById() {
+        assertThrows(EmptyResultDataAccessException.class, ()->{
+            userDao.findById("100");
+        });
+    }
+
+
+
+
 }
