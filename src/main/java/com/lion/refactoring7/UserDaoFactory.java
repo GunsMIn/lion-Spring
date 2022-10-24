c8eb1830d27b17a119e6fe38a367cfda50b2821a
KimGunWoo9595 <medneter@naver.com>
2022-10-24 13:30:38 +0900

    DataSource

--- src/main/java/com/lion/refactoring7/UserDaoFactory.java
+++ src/main/java/com/lion/refactoring7/UserDaoFactory.java
@@ -2,6 +2,10 @@ package com.lion.refactoring7;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.util.Map;
 
 @Configurable
 public class UserDaoFactory {
@@ -11,6 +15,17 @@ public class UserDaoFactory {
         return new UserDao(new AwsConnectionMaker());
     }
 
+
+    @Bean
+    public DataSource awsDataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        Map<String, String> env = System.getenv();
+        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        dataSource.setUrl(env.get("DB_HOST"));
+        dataSource.setUsername(env.get("DB_USER"));
+        dataSource.setPassword(env.get("DB_PASSWORD"));
+        return dataSource;
+    }
 
 
 }
