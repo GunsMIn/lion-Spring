4401d9c4b974a7c781839cbef972aa9cf38038da
KimGunWoo9595 <medneter@naver.com>
2022-10-24 13:36:01 +0900

    jdbcConnection

--- src/main/java/com/lion/refactoring7/UserDao.java
+++ src/main/java/com/lion/refactoring7/UserDao.java
@@ -3,124 +3,115 @@ package com.lion.refactoring7;
 import com.lion.domain.User;
 import org.springframework.dao.EmptyResultDataAccessException;
 
+import javax.sql.DataSource;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class UserDao {
+    private final DataSource dataSource;  //DataSource를 의존하게 변경
+    private final JdbcContext jdbcContext;
 
-    private ConnectionMaker cm;
-    public UserDao() {
-        this.cm = new AwsConnectionMaker();
+    public UserDao(DataSource dataSource) {     // 생성자도 변경
+        this.dataSource = dataSource;
+        this.jdbcContext = new JdbcContext(dataSource);
     }
 
-    public UserDao(ConnectionMaker cm) {
-        this.cm = cm;
+    public void add(final User user) throws SQLException, ClassNotFoundException {
+        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection conn) throws SQLException {
+                PreparedStatement pstmt = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
+                pstmt.setString(1, user.getId());
+                pstmt.setString(2, user.getName());
+                pstmt.setString(3, user.getPassword());
+
+                return pstmt;
+            }
+        });
     }
 
-    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        try {
-            c = cm.makeConnection();
-            ps = stmt.makePreparedStatement(c);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
+    public User findbyId(String id) throws SQLException, ClassNotFoundException {
+        Connection conn = dataSource.getConnection();
+        PreparedStatement ps = conn.prepareStatement("SELECT * from users where id = ?");
+        ps.setString(1, id);
+        ResultSet rs = ps.executeQuery();
+
+        User user = null;
+        if(rs.next()){
+            user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
         }
+        rs.close();
+        ps.close();
+        conn.close();
+
+        if (user == null) throw new EmptyResultDataAccessException(1);
+
+        return user;
     }
 
-    public void deleteAll() throws SQLException {
-        jdbcContextWithStatementStrategy(new DeleteAllStrategy());
+    public List<User> findAll() throws SQLException, ClassNotFoundException {
+        Connection conn = dataSource.getConnection();
+        PreparedStatement ps = conn.prepareStatement("SELECT * from users");
+        ResultSet rs = ps.executeQuery();
+
+        List<User> userList = new ArrayList<>();
+
+        while(rs.next()){
+            User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+            userList.add(user);
+        }
+        rs.close();
+        ps.close();
+        conn.close();
+
+        return userList;
     }
 
-    public int getCount() throws SQLException {
-        Connection c = null;
+    public void deleteAll() throws SQLException, ClassNotFoundException {
+        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection conn) throws SQLException {
+                return conn.prepareStatement("delete from users");
+            }
+        });
+    }
+
+    public int getCount() throws SQLException, ClassNotFoundException {
+        Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
+
         try {
-            c = cm.makeConnection();
-            ps = c.prepareStatement("select count(*) from users");
+            conn = dataSource.getConnection();
+
+            ps = conn.prepareStatement("SELECT count(*) FROM users");
+
             rs = ps.executeQuery();
             rs.next();
             return rs.getInt(1);
         } catch (SQLException e) {
             throw e;
         } finally {
-            if (rs != null) {
+            if(rs != null){
                 try {
                     rs.close();
                 } catch (SQLException e) {
                 }
             }
-            if (ps != null) {
+            if(ps != null) {
                 try {
                     ps.close();
                 } catch (SQLException e) {
                 }
             }
-            if (c != null) {
+            if(conn != null){
                 try {
-                    c.close();
+                    conn.close();
                 } catch (SQLException e) {
                 }
             }
         }
-    }
-
-    public void add(User user) throws SQLException {
-        AddStrategy addStrategy = new AddStrategy(user);
-        jdbcContextWithStatementStrategy(addStrategy);
-    }
-
-    public User findById(String id) {
-        Map<String, String> env = System.getenv();
-        Connection c;
-        try {
-            // DB접속 (ex sql workbeanch실행)
-            c = cm.makeConnection();
-
-            // Query문 작성
-            PreparedStatement pstmt = c.prepareStatement("SELECT * FROM users WHERE id = ?");
-            pstmt.setString(1, id);
-
-            // Query문 실행
-            ResultSet rs = pstmt.executeQuery();
-            User user = null;
-            if (rs.next()) {
-                user = new User(rs.getString("id"), rs.getString("name"),
-                        rs.getString("password"));
-            }
-
-            rs.close();
-            pstmt.close();
-            c.close();
-
-            if (user == null) throw new EmptyResultDataAccessException(1);
-
-            return user;
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void main(String[] args) {
-        UserDao userDao = new UserDao();
-//        userDao.add();
-        User user = userDao.findById("6");
-        System.out.println(user.getName());
     }
 }
